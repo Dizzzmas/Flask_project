0951a1d359a8bb341e710792c62788bc5e4b51eb
Dizzzmas <yankovskyi.dmytro736@gmail.com>
2019-08-20 00:24:36 +0300

    unit tests and followers

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])             # Form for logging in users with validators
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
@@ -13,12 +13,12 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired(), Email()])  # Form for registering users
     password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
 
-    def validate_username(self, username):
+    def validate_username(self, username):                              # validate_<something> auto_run on submit
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError("Such user already exists")
@@ -33,3 +33,13 @@ class EditProfileForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError("Please use a different username. ")
--- app/models.py
+++ app/models.py
@@ -6,19 +6,30 @@ from app import login
 from hashlib import md5
 
 
+followers = db.Table("followers",            # Followers many to many table
+                     db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+                     db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+                     )
+
+
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+    return User.query.get(int(id))          # For logging in users
 
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model):                            # Database user model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')       # One-to-many relationship, backref for getting the user who posted
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        "User", secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"), lazy="dynamic")
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -33,12 +44,35 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
 
-class Post(db.Model):
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
+class Post(db.Model):                               # Post db model
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))                   # Foreign key
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+
